2407e12ee7a2b474def04fd95cdfbe0adcdc660e
Alexander Vasiliev <selado@ya.ru>
2020-09-05 03:08:33 +0300

    fix button

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,14 @@
-import '../pages/index.css';
+'use strict';
+
+(function() {
+
+  const readMoreButton = document.querySelector('.button_black');
+
+  function noMore() {
+    readMoreButton.textContent = 'No more';
+    readMoreButton.removeEventListener('click', noMore);
+  }
+
+  readMoreButton.addEventListener('click', noMore);
+
+})();
